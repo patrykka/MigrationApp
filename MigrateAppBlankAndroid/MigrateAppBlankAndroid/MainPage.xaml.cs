fccb34df1208096c6b104fbd211345d3bf118aa5
Patryk Kaczmarek <mailkaczmarek@gmail.com>
2021-10-21 02:08:46 +0200

    Add btn

--- MigrateAppBlankAndroid/MigrateAppBlankAndroid/MainPage.xaml.cs
+++ MigrateAppBlankAndroid/MigrateAppBlankAndroid/MainPage.xaml.cs
@@ -14,5 +14,14 @@ namespace MigrateAppBlankAndroid
         {
             InitializeComponent();
         }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            btn.Text = "Clicked";
+        }
+
+        void btn_SizeChanged(System.Object sender, System.EventArgs e)
+        {
+        }
     }
 }
